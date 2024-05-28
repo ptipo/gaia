5ec5d9eedb68b49f26a2bdc94d448ebcd1ca8d3a
ymc9 <104139426+ymc9@users.noreply.github.com>
2024-05-28 20:44:32 +0800

    chore: add app build configuration for configurator-vue

---
+++ packages/configurator-vue/vite-app.config.ts
@@ -0,0 +1,44 @@
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'node:path';
+import AutoImport from 'unplugin-auto-import/vite';
+import IconsResolver from 'unplugin-icons/resolver';
+import Icons from 'unplugin-icons/vite';
+// import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [
+                // ElementPlusResolver(),
+                IconsResolver(),
+            ],
+        }),
+        Components({
+            resolvers: [
+                IconsResolver({
+                    enabledCollections: ['ep'],
+                }),
+                // ElementPlusResolver(),
+            ],
+        }),
+        Icons({
+            autoInstall: true,
+        }),
+    ],
+
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src'),
+            '@components': resolve(__dirname, './src/components'),
+            '@assets': resolve(__dirname, './src/assets'),
+        },
+    },
+
+    build: {
+        outDir: 'app-dist',
+    },
+});
