f1d045184f789241e97ce629fac0cbb6b1552c06
ymc9 <104139426+ymc9@users.noreply.github.com>
2024-06-04 17:53:23 +0800

    fix: change supa-form's build to include both the custom element and config

---
+++ demo/supa-form/src/index.ts
@@ -0,0 +1,2 @@
+export { FormApp as config } from './config';
+export { PtForm as app } from './component/pt-form';
--- demo/supa-form/vite.config.ts
+++ demo/supa-form/vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 const config: UserConfig = {
     build: {
         lib: {
-            entry: resolve(__dirname, './src/component/pt-form.ts'),
+            entry: resolve(__dirname, './src/index.ts'),
             name: 'PtForm',
             fileName: 'index',
         },
