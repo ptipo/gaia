2490b09c88bd4ae5cba71c9f6e2e7521d6042846
JG <jiashengguo@outlook.com>
2024-06-14 22:08:05 +0800

    feat(form): image choice design

--- demo/supa-form/src/component/contentTypes/pt-choice.ts
+++ demo/supa-form/src/component/contentTypes/pt-choice.ts
@@ -2,12 +2,16 @@ import { html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { PtBaseData } from '../pt-base';
 import { AllPageItemsTypesMap } from '../../config/page-items';
-import { ImageInfo } from '@hayadev/configurator/items';
+import { when } from 'lit/directives/when.js';
+
+type ChoiceQuestionType = AllPageItemsTypesMap['ChoiceQuestion'];
+
+type GetElementType<T extends any[]> = T extends (infer U)[] ? U : never;
 
 @customElement('pt-choice')
 export class PtChoice extends PtBaseData<Map<string, string>> {
     @property({ type: Object })
-    data?: AllPageItemsTypesMap['ChoiceQuestion'];
+    data?: ChoiceQuestionType;
 
     randomSeed?: number[];
 
@@ -16,6 +20,23 @@ export class PtChoice extends PtBaseData<Map<string, string>> {
         if (this.data?.randomOrder) {
             this.randomSeed = Array.from(this.data!.textChoices!, (x) => Math.random() - 0.5);
         }
+    }
+
+    getInputComponent(
+        choice: GetElementType<NonNullable<ChoiceQuestionType['textChoices'] | ChoiceQuestionType['imageChoices']>>,
+        isSingleChoice: boolean
+    ) {
+        return html` <input
+            id="${choice.$id}"
+            type="${isSingleChoice ? 'radio' : 'checkbox'}"
+            name="choices"
+            value="${choice.value}"
+            ?checked=${this.value ? this.value.data!.has(choice.$id) : choice.defaultSelected}
+            @change=${(e: any) => this.onChange(e)}
+            class="mr-2 mt-1 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600"
+            checked
+            required
+        />`;
     }
 
     render() {
@@ -45,49 +66,78 @@ export class PtChoice extends PtBaseData<Map<string, string>> {
 
         const isFlat = this.data?.flatMode as boolean;
         const description = this.data?.description;
+        const isShowTextWithImage = this.data?.showTextWithImage;
 
         return html`
             <fieldset class="flex">
                 <legend class="mb-8 text-2xl  text-black font-black	">${this.data?.question}</legend>
                 ${description ? html`<p class="mt-2 text-s text-gray-600">${description}</p>` : ''}
-                <div class="flex-auto flex gap-4 ${isFlat ? '' : 'flex-col'}">
+                <div class="flex flex-auto flex-wrap gap-4 ${isFlat ? '' : 'flex-col'} items-stretch">
                     ${choices!.map(
-                        (x) => html`
-                            <div class="flex-auto">
-                                <label
-                                    class="flex items-center  border rounded-md border-gray-300 p-2 has-[:checked]:bg-gray-100 has-[:checked]:border-black hover:bg-gray-100 cursor-pointer transition"
-                                >
-                                    <input
-                                        id="${x.$id}"
-                                        type="${isSingleChoice ? 'radio' : 'checkbox'}"
-                                        name="choices"
-                                        value="${x.value}"
-                                        ?checked=${this.value ? this.value.data!.has(x.$id) : x.defaultSelected}
-                                        @change=${(e: any) => this.onChange(e)}
-                                        class=" w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600"
-                                        checked
-                                        required
-                                    />
-                                    ${isImageChoice
-                                        ? html` <img
-                                              class="max-w-sm object-cover object-center"
-                                              src="${(x.value as ImageInfo).url}"
-                                          />`
-                                        : html`<label
-                                              for="${x.$id}"
-                                              class="block ms-2  text-sm font-medium text-gray-900 dark:text-gray-300"
-                                          >
-                                              ${x.value}
-                                          </label>`}
-                                </label>
-                                ${x.additionalInput && this.value.data!.has(x.$id)
-                                    ? html`<textarea
-                                          data-choice-id="${x.$id}"
-                                          @input=${this.onInputChange}
-                                          .value=${this.value.data?.get(x.$id)!}
-                                      ></textarea>`
-                                    : ''}
-                            </div>
+                        (choice, index) => html`
+                            ${when(
+                                isImageChoice,
+                                () =>
+                                    html`
+                                        <div
+                                            class="flex flex-auto gap-y-4 flex-col justify-center items-center ${isFlat
+                                                ? 'w-60'
+                                                : 'max-w-full'}  border rounded-md border-gray-300 p-2 has-[:checked]:bg-gray-100 has-[:checked]:border-black hover:bg-gray-100  transition"
+                                        >
+                                            <div class="flex flex-auto items-center">
+                                                <label
+                                                    for="${choice.$id}"
+                                                    class="flex items-center flex-col  gap-y-4 cursor-pointer"
+                                                >
+                                                    <div>
+                                                        <img
+                                                            class="max-w-full max-h-full h-auto"
+                                                            src="${index % 2
+                                                                ? 'https://zenstack.dev/assets/images/cover-64ee3034e0ba5cd8e94748a9421acb3c.png'
+                                                                : 'https://cdnimg.vivaia.com/VA/image/Banner/20240513_6837/13.png'}"
+                                                        />
+                                                    </div>
+                                                </label>
+                                            </div>
+                                            <div
+                                                class="flex ${isShowTextWithImage
+                                                    ? 'justify-start'
+                                                    : 'justify-center'}  mt-2 w-full ${isFlat && isShowTextWithImage
+                                                    ? 'min-h-12'
+                                                    : ''} "
+                                            >
+                                                ${this.getInputComponent(choice, isSingleChoice)}
+                                                ${when(
+                                                    this.data?.showTextWithImage,
+                                                    () => html`<label>${choice.name}</label>`
+                                                )}
+                                            </div>
+                                        </div>
+                                    `,
+                                () =>
+                                    html`
+                                        <div class="flex-auto">
+                                            <label
+                                                class="flex items-center  border rounded-md border-gray-300 p-2 has-[:checked]:bg-gray-100 has-[:checked]:border-black hover:bg-gray-100 cursor-pointer transition"
+                                            >
+                                                ${this.getInputComponent(choice, isSingleChoice)}
+                                                <label
+                                                    for="${choice.$id}"
+                                                    class="block ms-2  text-sm font-medium text-gray-900 dark:text-gray-300"
+                                                >
+                                                    ${choice.value}
+                                                </label>
+                                            </label>
+                                            ${choice.additionalInput && this.value.data!.has(choice.$id)
+                                                ? html`<textarea
+                                                      data-choice-id="${choice.$id}"
+                                                      @input=${this.onInputChange}
+                                                      .value=${this.value.data?.get(choice.$id)!}
+                                                  ></textarea>`
+                                                : ''}
+                                        </div>
+                                    `
+                            )}
                         `
                     )}
                 </div>
