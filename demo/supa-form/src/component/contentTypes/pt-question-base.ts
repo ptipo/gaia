897a66cd2bd917300fb9aaece044de5e39551874
Jiasheng <jiashengguo@outlook.com>
2024-07-10 21:01:28 +0800

    fix(form): text style adjustment (#111)
    * fix(form): font size adjustment
    
    * fix(form): make font bold for h1/h2/h3

--- demo/supa-form/src/component/contentTypes/pt-question-base.ts
+++ demo/supa-form/src/component/contentTypes/pt-question-base.ts
@@ -22,12 +22,12 @@ export class PtQuestionBase extends PtBase {
 
     render() {
         return html`
-            <span class="text-2xl font-bold">${this.question}</span>
+            <span class="text-xl font-bold">${this.question}</span>
             ${this.description ? html`<p class="mt-1 text-sm font-normal">${this.description}</p>` : ''}
             <div class="mt-4">
                 <span>
                     <input
-                        class="w-full text-lg border-b-gray-400 focus:border-gray-400 appearance-none bg-transparent text-[0.72em] font-black ml-0 mr-[0.6em] my-0 px-[0.2em] py-[0.16em] rounded-none border-0 border-b border-solid outline-none "
+                        class="w-full text-sm text-black placeholder-gray-400 border-b-gray-400 focus:border-gray-400 appearance-none bg-transparent  ml-0 mr-[0.6em] my-0 px-[0.2em] py-[0.16em] rounded-none border-0 border-b border-solid outline-none "
                         value=${ifDefined(this.value)}
                         placeholder=${ifDefined(this.placeholder)}
                         type=${this.inputType}
