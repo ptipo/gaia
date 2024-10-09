f293e33b6b8c7df1b836d5ebc7bac25d62adf8f5
JG <jiashengguo@outlook.com>
2024-10-09 11:55:10 +0800

    feat(form): support Korea language

--- demo/supa-form/src/component/localization.ts
+++ demo/supa-form/src/component/localization.ts
@@ -5,12 +5,14 @@ import * as templates_zh_cn from '../generated/locales/zh-CN.js';
 import * as templates_ja_jp from '../generated/locales/ja-JP.js';
 import * as templates_de_de from '../generated/locales/de-DE.js';
 import * as templates_fr_fr from '../generated/locales/fr-FR.js';
+import * as templates_ko_kr from '../generated/locales/ko-KR.js';
 
 const localizedTemplates = new Map([
     ['zh-CN', templates_zh_cn],
     ['ja-JP', templates_ja_jp],
     ['de-DE', templates_de_de],
     ['fr-FR', templates_fr_fr],
+    ['ko-KR', templates_ko_kr],
 ]);
 
 export const { getLocale, setLocale } = configureLocalization({
---
+++ demo/supa-form/src/generated/locales/ko-KR.ts
@@ -0,0 +1,20 @@
+
+    // Do not modify this file by hand!
+    // Re-generate this file by running lit-localize
+
+    
+    
+
+    /* eslint-disable no-irregular-whitespace */
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+
+    export const templates = {
+      's0bb7ca461c368945': `추가 정보를 제공하십시오`,
+'s618f82f6f2816e6c': `유효한 이메일을 입력하십시오`,
+'s6fc73ba7929aa5d6': `선택해 주세요`,
+'sa2e67553ecfa8d54': `약관에 동의하십시오`,
+'sc16e00a7a8b2fde2': `뒤로`,
+'se8cd0d023e89f577': `이것을 기입하십시오`,
+'sfa236dc6043a5fe8': `다음`,
+    };
+  
